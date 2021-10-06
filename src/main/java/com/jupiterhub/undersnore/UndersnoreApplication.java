16d9f13f687550cdb4817ee2a97690e2af1c9a9f
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2021-10-06 20:31:22 +0900

    Initial commit

---
+++ src/main/java/com/jupiterhub/undersnore/UndersnoreApplication.java
@@ -0,0 +1,13 @@
+package com.jupiterhub.undersnore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndersnoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndersnoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupiterhub/undersnore/UndersnoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupiterhub.undersnore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndersnoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
